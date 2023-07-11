d43eec6b983f341de9f774c22c3c45f3bcbb73ae
Laure CROCHEPIERRE <laure.crochepierre@rte-france.com>
2023-07-11 15:19:51 +0200

    make some of the requested changes for alert baseline agent

--- grid2op/Agent/alertAgent.py
+++ grid2op/Agent/alertAgent.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, RTE (https://www.rte-france.com)
+# Copyright (c) 2019-2023, RTE (https://www.rte-france.com)
 # See AUTHORS.txt
 # This Source Code Form is subject to the terms of the Mozilla Public License, version 2.0.
 # If a copy of the Mozilla Public License, version 2.0 was not distributed with this file,
@@ -7,19 +7,11 @@
 # This file is part of Grid2Op, Grid2Op a testbed platform to model sequential decision making in power systems.
 import numpy as np
 from grid2op.Agent.recoPowerlineAgent import RecoPowerlineAgent
+from grid2op.Agent.baseAgent import BaseAgent
 import pandas as pd
 
-#try:
-#    from lightsim2grid import LightSimBackend
-#
-#    bkclass = LightSimBackend
-#    # raise ImportError()
-#except ImportError as excq_:
-#    from grid2op.Backend import PandaPowerBackend
 
-
-
-class AlertAgent(RecoPowerlineAgent):
+class AlertAgent(BaseAgent):
     """
     This is a :class:`AlertAgent` example, which will attempt to reconnect powerlines and send alerts on the worst possible attacks: for each disconnected powerline
     that can be reconnected, it will simulate the effect of reconnecting it. And reconnect the one that lead to the
@@ -27,26 +19,39 @@ class AlertAgent(RecoPowerlineAgent):
 
     """
 
-    def __init__(self, action_space,percentage_alert=30,simu_step=0):
-        RecoPowerlineAgent.__init__(self, action_space)
+    def __init__(self, action_space,percentage_alert=30,simu_step=0,controler=RecoPowerlineAgent):
+        super().__init__(action_space=action_space)
         self.percentage_alert = percentage_alert
         self.simu_step=simu_step
+        if isinstance(controler, type):
+            # controler is passed as a class, and not as an object
+            self.controler = controler(action_space)
+        else:
+            self.controler = controler()
 
-    def act(self, observation, reward, done=False):
-        action=super().act(observation, reward, done=False)
-        alertable_line_ids=observation.alertable_line_ids
+        self.alertable_line_ids=self.action_space.alertable_line_ids
 
         #simu d'analyse de sécurité à chaque pas de temps sur les lignes attaquées
-        n_alertable_lines=len(alertable_line_ids)
-        nb_overloads=np.zeros(n_alertable_lines)
-        rho_max_N_1=np.zeros(n_alertable_lines)
+        self.n_alertable_lines=len(self.alertable_line_ids)
+
+        self.N_1_actions=[self.action_space({"set_line_status": [(id_, -1)]}) for id_ in self.alertable_line_ids]
+
+    
+    def reset(self, obs): 
+        
+        self.nb_overloads=np.zeros(self.n_alertable_lines)
+        self.rho_max_N_1=np.zeros(self.n_alertable_lines)
+
+    def act(self, observation, reward, done=False):
+        action=self.controler.act(observation, reward, done=done)
+        
+        #simu d'analyse de sécurité à chaque pas de temps sur les lignes attaquées
 
         # test which backend to know which method to call
-        N_1_actions=[self.action_space({"set_line_status": [(id_, -1)]}) for id_ in alertable_line_ids]
-        for i, action_to_simulate in enumerate(N_1_actions):
+        for i, action_to_simulate in enumerate(self.N_1_actions):
 
             #check that line is not already disconnected
-            if (observation.line_status[alertable_line_ids[i]]):
+            if (observation.line_status[self.alertable_line_ids[i]]):
                 (
                     simul_obs,
                     simul_reward,
@@ -56,14 +61,13 @@ class AlertAgent(RecoPowerlineAgent):
 
                 rho_simu=simul_obs.rho
                 if(not simul_has_error):
-                    nb_overloads[i]=np.sum(rho_simu >= 1)
-                    rho_max_N_1[i]=np.max(rho_simu)
+                    self.nb_overloads[i]=np.sum(rho_simu >= 1)
+                    self.rho_max_N_1[i]=np.max(rho_simu)
 
-        df_to_sort=pd.DataFrame({"nb_overloads":nb_overloads,"rho_max_N_1":rho_max_N_1})
-        indices=df_to_sort.sort_values(['nb_overloads','rho_max_N_1'], ascending=False).index
+        indices = self.rho_max_N_1.argsort(axis=0)[::-1]
 
         #alerts to send
-        indices_to_keep=list(indices[0:int(self.percentage_alert/100*n_alertable_lines)])
+        indices_to_keep=list(indices[0:int(self.percentage_alert/100*self.n_alertable_lines)])
         action.raise_alert = [i for i in indices_to_keep]
 
         return action
--- grid2op/tests/test_baseline_alert.py
+++ grid2op/tests/test_baseline_alert.py
@@ -33,7 +33,41 @@ class TestAlertNoBlackout(unittest.TestCase):
             # test if the number of alerts sent on lines are recovered
             alerts_count =np.sum([obs.active_alert for obs in episode_data.observations[1:]]
                                   ,axis=0)
-            print(alerts_count)
+            assert(np.all(alerts_count==[3, 3, 2, 0, 1, 0, 0, 0, 0, 0]))
+
+            # test that we observe the expected alert rate
+            nb_alertable_lines =len(env.alertable_line_names)
+            ratio_alerts_step =np.sum(alerts_count ) /(nb_time_step*nb_alertable_lines)
+            assert(np.round(ratio_alerts_step ,decimals=1 )==np.round(percentage_alert/100 ,decimals=1))
+
+            #check that alert agent is not doing any intervention on the grid in this short time frame
+            #as the reco power line, it should only do actions to reconnect lines when allowed, but cannot happen in this short time frame
+            has_action_impact=[act.impact_on_objects()['has_impact'] for act in episode_data.actions]
+            assert(~np.any(has_action_impact))
+
+
+        
+    def test_AlertAgent_action_cut(self) -> None:
+        with make(
+                self.env_nm,
+                test=True,
+                difficulty="1",
+                reward_class=AlertReward(reward_end_episode_bonus=42)
+        ) as env:
+            env.seed(0)
+            env.reset()
+
+            percentage_alert =30  # 30% of lines with alert per step
+            my_agent = AlertAgent(env.action_space, percentage_alert=percentage_alert)
+            runner = Runner(**env.get_params_for_runner(), agentClass=None ,agentInstance=my_agent)
+
+            res = runner.run(nb_episode=1, nb_process=1, path_save=None,agent_seeds=[0],env_seeds=[0],max_iter=3,
+                             add_detailed_output=True)
+            id_chron, name_chron, cum_reward, nb_time_step, max_ts, episode_data = res[0]
+
+            # test if the number of alerts sent on lines are recovered
+            alerts_count =np.sum([obs.active_alert for obs in episode_data.observations[1:]]
+                                  ,axis=0)
             assert(np.all(alerts_count==[3, 3, 2, 0, 1, 0, 0, 0, 0, 0]))
 
             # test that we observe the expected alert rate
